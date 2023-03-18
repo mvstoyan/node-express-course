a49608ed5a812e82fc12aa543dba11082dc385c0
Marina Tilton <m.v.stoyan@gmail.com>
2023-03-17 20:16:24 -0700

    answers for lesson 3

---
+++ 02-express-tutorial/01-http-basics.js
@@ -0,0 +1,34 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  // console.log(req.method)
+  const url = req.url
+  // home page
+  if (url === '/') {
+    res.writeHead(200, { 'content-type': 'text/html' }) //200 The request succeeded. 
+    res.write('<h1>home page</h1>')
+    res.end()
+  }
+  // about page
+  else if (url === '/about') {
+    res.writeHead(200, { 'content-type': 'text/html' })
+    res.write('<h1>about page</h1>')
+    res.end()
+  }
+  // 404 - not found. The server cannot find the requested resource. 
+  // In the browser, this means the URL is not recognized.
+  else {
+    res.writeHead(404, { 'content-type': 'text/html' })
+    res.write('<h1>page not found</h1>')
+    res.end()
+  }
+})
+
+server.listen(5000)
+
+
+// Informational responses (100 – 199)
+// Successful responses (200 – 299)
+// Redirection messages (300 – 399)
+// Client error responses (400 – 499)
+// Server error responses (500 – 599)
---
+++ 02-express-tutorial/practiceExpress.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './new-public/index.html'))
+  })
+  
+  app.get('/sample', (req, res) => {
+    res.status(200).send('This is working.')
+  })
+
+  app.listen(3000, () => {
+    console.log('server is listening on port 3000....')
+  })
